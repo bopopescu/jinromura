2db373a3e865751c86039f1f54497919025e2525
高山雄貴 <yuki-t@takayamayuukinoMacBook-puro.local>
2018-09-10 21:24:36 +0900

    index,detail画面にmodels.pyの要素を表示

--- jinromura/views.py
+++ jinromura/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
 from .models import Village,Character
 
 # Create your views here.
+#検索・一覧画面
 def index(request):
-    return HttpResponse("Hello world.")
+    village_list = Village.objects.all()
+    context={'village_list':village_list}
+    return render(request, 'jinromura/index.html',context)
 
-def detail(request):
-    return HttpResponse("Hello world.")
+#詳細画面
+def detail(request,village_id):
+    #オブジェクト（各々の村）取得か４０４エラー
+    village = get_object_or_404(Village,pk=village_id)
+    return render(request, 'jinromura/detail.html',{'village':village})
