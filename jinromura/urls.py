9761d7e43fb79925745ec6a43cc7cd898c2238d7
高山雄貴 <yuki-t@takayamayuukinoMacBook-puro.local>
2018-09-10 20:17:27 +0900

    URL,VIEWの初期設定

--- jinromura/urls.py
+++ jinromura/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #検索画面
     path('', views.index, name='index'),
+    #村の詳細画面
+    path('<int:village_id>/', views.detail, name='detail'),
 ]
