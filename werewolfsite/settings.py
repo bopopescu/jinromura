53ebc9eb5b4c505d41b73637c5b7ef993f9bc39a
高山雄貴 <yuki-t@takayamayuukinoMacBook-puro.local>
2018-09-12 21:34:59 +0900

    bootstrap設定反映

--- werewolfsite/settings.py
+++ werewolfsite/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+#bootstrap導入 staticのディレクトリ指定
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR, 'static'),
+)
