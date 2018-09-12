6d459253b815de4cb1db1b36f8f491b82feab390
高山雄貴 <yuki-t@takayamayuukinoMacBook-puro.local>
2018-09-12 21:32:08 +0900

    bootstrapの導入

--- jinromura/views.py
+++ jinromura/views.py
@@ -9,6 +9,7 @@ def index(request):
     return render(request, 'jinromura/index.html',context)
 
 #詳細画面
+#URLの指定にvillage_idが必要なので追加
 def detail(request,village_id):
     #オブジェクト（各々の村）取得か４０４エラー
     village = get_object_or_404(Village,pk=village_id)
