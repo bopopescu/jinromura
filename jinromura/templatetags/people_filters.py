44e3499d945f37de789ecfbb10159b8f4c04d466
高山雄貴 <yuki-t@takayamayuukinoMacBook-puro.local>
2018-09-21 21:28:08 +0900

    村検索機能:人数絞り込み

---
+++ jinromura/templatetags/people_filters.py
@@ -0,0 +1,9 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def people(querydict):
+    people = querydict.get('people')
+
+    return "" if people is None else people
--- jinromura/views.py
+++ jinromura/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Village,Character
-from . import forms
 from django.views import generic
 
 # Create your views here.
 #初期検索・一覧画面
 class IndexView(generic.ListView):
+    model = Village
     template_name = 'jinromura/index.html'
 
     def get_queryset(self):
         #人数順に昇順、デフォルト全件取得
-        return Village.objects.all().order_by('people')
+        village_list=self.model.objects.all().order_by('people')
+
+        q_people = self.request.GET.get('people')
+
+        if q_people is not None:
+            village_list =village_list.filter(people=q_people)
+
+        return village_list
 
 class DetailView(generic.DetailView):
     model = Village
