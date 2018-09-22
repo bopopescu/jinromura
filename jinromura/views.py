3090851feec48407d73090136a538c825203582f
高山雄貴 <yuki-t@takayamayuukinoMacBook-puro.local>
2018-09-22 15:30:29 +0900

    index時に一覧が正常に表示されない問題の解消

--- jinromura/views.py
+++ jinromura/views.py
@@ -8,14 +8,24 @@ class IndexView(generic.ListView):
     model = Village
     template_name = 'jinromura/index.html'
 
+#indexで表示した時の
+    def get_context_data(self, **kwargs):
+        context = super(IndexView,self).get_context_data(**kwargs)
+
+        village_list = Village.objects.all().order_by('people')
+        context['village_list']=village_list
+
+        return context
+
+#絞り込みボタンをクリックした時の表示
     def get_queryset(self):
         #人数順に昇順、デフォルト全件取得
-        village_list=self.model.objects.all().order_by('people')
+        village_list = self.model.objects.all().order_by('people')
 
         q_people = self.request.GET.get('people')
 
-#peopleがnullのとき""を返すため
-        if q_people != "":
+        #peopleがnullのとき""を返すため
+        if q_people !="":
             #検索を繰り返す時に全件から絞り込みを行う
             village_list =self.model.objects.all().order_by('people').filter(people=q_people)
 
