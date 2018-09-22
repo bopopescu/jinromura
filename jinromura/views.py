4db60653d2268d66fac15a0e168c8bab61fee0bb
高山雄貴 <yuki-t@takayamayuukinoMacBook-puro.local>
2018-09-22 17:11:39 +0900

    表示件数数字縛り

--- jinromura/views.py
+++ jinromura/views.py
@@ -8,16 +8,8 @@ class IndexView(generic.ListView):
     model = Village
     template_name = 'jinromura/index.html'
 
-#indexで表示した時の
-    def get_context_data(self, **kwargs):
-        context = super(IndexView,self).get_context_data(**kwargs)
 
-        village_list = Village.objects.all().order_by('people')
-        context['village_list']=village_list
-
-        return context
-
-#絞り込みボタンをクリックした時の表示
+    #絞り込みボタンをクリックした時の表示
     def get_queryset(self):
         #人数順に昇順、デフォルト全件取得
         village_list = self.model.objects.all().order_by('people')
