fb918ac781df564b8a6b4bdfd26395b8eea2ed28
高山雄貴 <yuki-t@takayamayuukinoMacBook-puro.local>
2018-09-24 21:29:25 +0900

    paginate実装

--- jinromura/views.py
+++ jinromura/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Village,Character
 from django.views import generic
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 #初期検索・一覧画面
 class IndexView(generic.ListView):
     model = Village
     template_name = 'jinromura/index.html'
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         #人数順に昇順ソート
-        village_list = self.model.objects.all().order_by('people')
-        context['village_list'] = village_list
+        object_list = self.model.objects.all().order_by('people')
+        context['village_list'] = object_list
 
         return context
 
